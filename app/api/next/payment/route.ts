5edabc776cd582994e61f70549f8c067f1ac3a87
NV-Phong <0h9ng03@gmail.com>
2025-09-13 19:44:50 +0700

    FEAT - get payment link

--- app/api/next/payment/route.ts
+++ app/api/next/payment/route.ts
@@ -1,53 +1,28 @@
 import { NextResponse } from "next/server";
-import crypto from "crypto";
+import { PayOS } from "@payos/node";
 
-const PAYOS_CLIENT_ID = process.env.PAYOS_CLIENT_ID!;
-const PAYOS_API_KEY = process.env.PAYOS_API_KEY!;
-const PAYOS_CHECKSUM_KEY = process.env.PAYOS_CHECKSUM_KEY!;
-const PAYOS_ENDPOINT = process.env.PAYOS_ENDPOINT!;
+const payOS = new PayOS({
+   clientId: process.env.PAYOS_CLIENT_ID,
+   apiKey: process.env.PAYOS_API_KEY,
+   checksumKey: process.env.PAYOS_CHECKSUM_KEY,
+});
 
 export async function POST(req: Request) {
    try {
-      const { amount, description } = await req.json();
-      const orderId = `ORDER_${Date.now()}`;
-      const payload = {
-         orderId,
+      const { orderCode, amount, description, items, returnUrl, cancelUrl } =
+         await req.json();
+
+      const paymentLinkResponse = await payOS.paymentRequests.create({
+         orderCode,
          amount,
          description,
-         cancelUrl: process.env.PAYMENT_CANCEL,
-         returnUrl: process.env.PAYMENT_SUCCESS,
-      };
-
-      const rawSignature = `amount=${payload.amount}&cancelUrl=${payload.cancelUrl}&description=${payload.description}&orderId=${payload.orderId}&returnUrl=${payload.returnUrl}${PAYOS_CHECKSUM_KEY}`;
-      const signature = crypto
-         .createHash("sha256")
-         .update(rawSignature)
-         .digest("hex");
-
-      const body = {
-         ...payload,
-         signature,
-      };
-
-      const response = await fetch(PAYOS_ENDPOINT, {
-         method: "POST",
-         headers: {
-            "x-client-id": PAYOS_CLIENT_ID,
-            "x-api-key": PAYOS_API_KEY,
-            "Content-Type": "application/json",
-         },
-         body: JSON.stringify(body),
+         items,
+         returnUrl,
+         cancelUrl,
       });
 
-      const data = await response.json();
-      return NextResponse.json(data);
-   } catch (err: unknown) {
-      let message = "Internal error";
-
-      if (err instanceof Error) {
-         message = err.message;
-      }
-
-      return NextResponse.json({ error: message }, { status: 500 });
+      return NextResponse.json(paymentLinkResponse);
+   } catch {
+      return NextResponse.json({ error: "Đã xảy ra lỗi" }, { status: 500 });
    }
 }
